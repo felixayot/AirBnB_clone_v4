6045f79001a22c60b87118303c2349da5f3c57d8
thinktank01 <felixayot@gmail.com>
2023-11-03 17:00:15 +0300

    Upload task 7(Fetch Reviews, show & hide jQuery functionalities)

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const api = 'http://' + window.location.hostname;
 
-  $.get(api + ':5001:/api/v1/status/', function (response) {
+  $.get(api + ':5001/api/v1/status/', function (response) {
     if (response.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
